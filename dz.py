2f8c392af3a16d566054f5768b94d171513b647a
Дмитрий Madara <madara@deeplay.io>
2022-01-31 17:54:05 +0600

    Rafactor dz

--- dz.py
+++ dz.py
@@ -5,24 +5,39 @@ class Parking:
     TYPE_MUNICIPAL_CAR = 3
 
     price_of_hour = 100
+    coeff_dor_municipal_car = 0.8
 
-    def __init__(self, type_car):
+    def __init__(self, type_car, dimensions=None):
+        if type_car not in [
+            self.TYPE_ORDINARY_CAR,
+            self.TYPE_TRUCK_CAR,
+            self.TYPE_MUNICIPAL_CAR
+        ]:
+            raise ValueError("The type_car value is specified incorrectly!")
+
+        if type_car == self.TYPE_TRUCK_CAR and not dimensions:
+            raise ValueError("Dimensions for the truck are not specified!")
+
+        if dimensions and type(dimensions) not in [int, float]:
+            raise ValueError("Error value dimensions!")
+
+        self.dimensions = dimensions
         self.type_car = type_car
 
     def get_price(self, hours):
         if type(hours) not in [int, float]:
             raise ValueError("Error value hours!")
 
+        if hours <= 0:
+            raise ValueError("The number of hours should be more null!")
+
         _text = "Price park: %f"
         if self.type_car == 1:
-            print("OK")
+            print(_text % round((self.price_of_hour * hours)))
 
         elif self.type_car == 2:
-            print("Not Found")
+            print(_text % round((self.price_of_hour * self.dimensions * hours)))
 
         elif self.type_car == 3:
-            print(_text % round((self.price_of_hour * 0.8 * hours)))
+            print(_text % round((self.price_of_hour * self.coeff_dor_municipal_car * hours)))
 
-
-park = Parking(Parking.TYPE_MUNICIPAL_CAR)
-park.get_price(24)
